99473cfdac39bea9257d154258ddd08d3e98a62e
RabihB <121131076+RabihB@users.noreply.github.com>
2023-02-07 18:05:57 +0200

    added box and ID text

--- QR reader.py
+++ QR reader.py
@@ -3,10 +3,10 @@ import numpy as np
 
 from pyzbar.pyzbar import decode
 
+#img = cv2.imread('images/image1.png')
 cap = cv2.VideoCapture(0)
 cap.set(3,648)
 cap.set(4,480)
-
 while True:
 
     success, img = cap.read()
@@ -18,7 +18,10 @@ while True:
         pts = np.array([barcode.polygon], np.int32)
         pts = pts.reshape((-1, 1, 2))
         cv2.polylines(img,[pts], True, (255,0,255), 5)
-
+        pts2 = barcode.rect
+        cv2.putText(img, myData, (pts2[0], pts2[1]),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.9, (255,0,255), 2)
+        
+        
     cv2.imshow('Result', img)
-    if cv2.waitKey(1) & 0XFF ==ord('q'):
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
         break
